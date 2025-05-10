b2f0b52d7673d21d5bef846f1167439ba85ac704
Camille Vauchel <camille.vauchel@neomi.immo>
2025-05-11 00:46:48 +0200

    Download certificate script

--- download_certificate.sh
+++ download_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 scp root@192.168.1.2:/etc/rancher/k3s/k3s.yaml .
+sed -i "s/127.0.0.1/192.168.1.2/g" k3s.yaml
---
+++ set_cluster.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp k3s.yaml ~/.kube/config
