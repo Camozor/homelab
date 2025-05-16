7c8358e782dea658d26c4f3df0289dc0c3492a21
Camille Vauchel <camille.vauchel@neomi.immo>
2025-05-16 15:56:00 +0200

    Scripts helper tailscale

--- download_certificate.sh
+++ download_certificate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-scp root@192.168.1.2:/etc/rancher/k3s/k3s.yaml .
-sed -i "s/127.0.0.1/192.168.1.2/g" k3s.yaml
+scp root@homelab:/etc/rancher/k3s/k3s.yaml .
+HOMELAB_IP=$(tailscale status | grep homelab | awk '{print $1}')
+sed -i "s/127.0.0.1/$HOMELAB_IP/g" k3s.yaml
