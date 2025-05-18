e527ad2306764f7f738d5a6e19db82824e28822e
Camille Vauchel <camille.vauchel@neomi.immo>
2025-05-18 15:02:57 +0200

    Vaultwarden

--- tailscale/register_homelab_node.sh
+++ tailscale/register_homelab_node.sh
@@ -3,6 +3,6 @@
 HOMELAB_DIR=$(git rev-parse --show-toplevel)
 source "$HOMELAB_DIR/.env"
 
-ssh root@192.168.1.2 "TOKEN=${TOKEN_HOMELAB_TAILSCALE} bash -s" <<'EOF'
+ssh root@homelab "TOKEN=${TOKEN_HOMELAB_TAILSCALE} bash -s" <<'EOF'
 		tailscale up --auth-key=$TOKEN
 EOF
