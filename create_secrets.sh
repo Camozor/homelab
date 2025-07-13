a87d85d6a7a0d7e35563a727b5179dbd2e967c51
Camille Vauchel <camille.vauchel@neomi.immo>
2025-07-13 17:45:52 +0200

    Rebot

--- create_secrets.sh
+++ create_secrets.sh
@@ -4,3 +4,6 @@ source .env
 
 kubectl create secret generic cloudflared-secret --namespace=cloudflared --dry-run=client --from-literal=token=$TOKEN_CLOUDFLARED -o yaml >k3s/cloudflared/cloudflared-secret-template.yml
 kubeseal --format=yaml --secret-file=k3s/cloudflared/cloudflared-secret-template.yml -w k3s/cloudflared/cloudflared-secret.yml
+
+kubectl create secret generic rebot-secret --namespace=rebot --dry-run=client --from-literal=DISCORD_TOKEN=$DISCORD_TOKEN --from-literal=DISCORD_SERVER_ID=$DISCORD_SERVER_ID -o yaml >k3s/rebot/rebot-secret-template.yml
+kubeseal --format=yaml --secret-file=k3s/rebot/rebot-secret-template.yml -w k3s/rebot/rebot-secret.yml
