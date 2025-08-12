300c63c09fe6ae2cb2a6687009e06d6a66657b7d
Camille Vauchel <camille.vauchel@neomi.immo>
2025-08-12 14:38:14 +0200

    Useless secret

--- create_secrets.sh
+++ create_secrets.sh
@@ -5,5 +5,5 @@ source .env
 kubectl create secret generic cloudflared-secret --namespace=cloudflared --dry-run=client --from-literal=token=$TOKEN_CLOUDFLARED -o yaml >k3s/cloudflared/cloudflared-secret-template.yml
 kubeseal --format=yaml --secret-file=k3s/cloudflared/cloudflared-secret-template.yml -w k3s/cloudflared/cloudflared-secret.yml
 
-kubectl create secret generic rebot-secret --namespace=rebot --dry-run=client --from-literal=DISCORD_TOKEN=$DISCORD_TOKEN --from-literal=DISCORD_SERVER_ID=$DISCORD_SERVER_ID --from-literal=ADMIN_API_KEY=$DISCORD_ADMIN_API_KEY -o yaml >k3s/rebot/rebot-secret-template.yml
+kubectl create secret generic rebot-secret --namespace=rebot --dry-run=client --from-literal=DISCORD_TOKEN=$DISCORD_TOKEN --from-literal=ADMIN_API_KEY=$DISCORD_ADMIN_API_KEY -o yaml >k3s/rebot/rebot-secret-template.yml
 kubeseal --format=yaml --secret-file=k3s/rebot/rebot-secret-template.yml -w k3s/rebot/rebot-secret.yml
